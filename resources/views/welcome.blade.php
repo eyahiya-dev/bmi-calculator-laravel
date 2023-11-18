c904f54aa1bd166e1321c85bb2a4027b31606061
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-18 23:39:49 +0600

    add front-end of BMI Calculator

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BMI Calculator</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div class="flex h-screen justify-center items-center bg-black">
         <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
             <form class="space-y-6" action="#">
-                <h5 class="text-3xl mb-10 text-center font-medium text-gray-900 dark:text-white">Basic Calculator</h5>
-                <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+                <h5 class="text-2xl mb-10 text-center font-medium text-gray-900 dark:text-white">Body Mass Index - BMI</h5>
+                <div class="text-center">
+                    <label for="age" class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Age</label>
+                    <input type="email" name="email" id="age" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 w-40 text-center p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                 </div>
                 <div>
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
